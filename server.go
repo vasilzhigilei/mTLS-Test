7e6ddd01a88df49280fc1a46b9b46aca9d365c8f
vasil <vasil@cloudflare.com>
2022-07-18 08:55:51 -0500

    minor code cleanup

--- server.go
+++ server.go
@@ -10,64 +10,7 @@ import (
 	"github.com/grantae/certinfo"
 )
 
-func handleError(err error) {
-	if err != nil {
-		log.Println("Error, ", err)
-	}
-}
-
-// readFile reads a file and returns the bytes
-func readFile(path string) ([]byte, error) {
-	// get cert filepath
-	absPathServerCrt, err := filepath.Abs(path)
-	if err != nil {
-		return nil, err
-	}
-	// read the cert file
-	certBytes, err := ioutil.ReadFile(absPathServerCrt)
-	if err != nil {
-		return nil, err
-	}
-	return certBytes, nil
-}
-
-// getOriginCertPair gets the origin cert pair for regular TLS
-func getOriginCertPair(certPath string, keyPath string) (*tls.Certificate, error) {
-	// get cert and key
-	originCert, err := readFile(certPath)
-	if err != nil {
-		return nil, err
-	}
-	originKey, err := readFile(keyPath)
-	if err != nil {
-		return nil, err
-	}
-
-	// make tls.Certificate
-	certificate, err := tls.X509KeyPair(originCert, originKey)
-	if err != nil {
-		return nil, err
-	}
-
-	return &certificate, nil
-}
-
-// getClientCACert gets the root certificate for the origin server side of mTLS
-func getClientCACert(path string) (*x509.CertPool, error) {
-	// get cert
-	clientCACert, err := readFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	// create cert pool
-	clientCertPool := x509.NewCertPool()
-	clientCertPool.AppendCertsFromPEM(clientCACert)
-
-	return clientCertPool, err
-}
-
-func serve() {
+func main() {
 	// get the origin server cert pair for TLS
 	certificate, err := getOriginCertPair("certs/origin.pem", "certs/origin.key")
 	handleError(err)
@@ -113,10 +56,10 @@ func serve() {
 		}
 
 		tlsConn.ConnectionState()
-		// get connection state and print some stuff
+		// get connection state and print certs sent by client
 		state := tlsConn.ConnectionState()
 		for _, v := range state.PeerCertificates {
-			text, err := certinfo.CertificateText(v) // this library can only give the entire human-readable cert, cannot select specific parts
+			text, err := certinfo.CertificateText(v) // this library can only give the entire human-readable cert, cannot specific select parts
 			if err != nil {
 				log.Printf("server: error converting cert to human-readable format, error: %s\n", err)
 			}
@@ -128,6 +71,59 @@ func serve() {
 	}
 }
 
-func main() {
-	serve()
+// getOriginCertPair gets the origin cert pair for regular TLS
+func getOriginCertPair(certPath string, keyPath string) (*tls.Certificate, error) {
+	// get cert and key
+	originCert, err := readFile(certPath)
+	if err != nil {
+		return nil, err
+	}
+	originKey, err := readFile(keyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// make tls.Certificate
+	certificate, err := tls.X509KeyPair(originCert, originKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &certificate, nil
+}
+
+// getClientCACert gets the root certificate for the origin server side of mTLS
+func getClientCACert(path string) (*x509.CertPool, error) {
+	// get cert
+	clientCACert, err := readFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// create cert pool
+	clientCertPool := x509.NewCertPool()
+	clientCertPool.AppendCertsFromPEM(clientCACert)
+
+	return clientCertPool, err
+}
+
+// readFile reads a file and returns the bytes
+func readFile(path string) ([]byte, error) {
+	// get cert filepath
+	absPathServerCrt, err := filepath.Abs(path)
+	if err != nil {
+		return nil, err
+	}
+	// read the cert file
+	certBytes, err := ioutil.ReadFile(absPathServerCrt)
+	if err != nil {
+		return nil, err
+	}
+	return certBytes, nil
+}
+
+func handleError(err error) {
+	if err != nil {
+		log.Println("Error, ", err)
+	}
 }
